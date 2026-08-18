09bd310a03e7e5e0c7de87faf6da225664777e50
Muhammet Emre Numanoglu <muhammedemrenumanoglu155@gmail.com>
2026-08-18 19:03:17 +0300

    Setup Tailwind , Saas And Hero UI

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Button } from '@heroui/react';
+
+export default function Home() {
+  return (
+   <div className="myColor bg-white">
+    <Button className="bg-primary">
+      My Button
+    </Button>
+   </div>
+  );
+}
