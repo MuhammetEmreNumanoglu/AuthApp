f6024347fee781e39bb6c0daf2b14b312e0af0c1
Muhammet Emre Numanoglu <muhammedemrenumanoglu155@gmail.com>
2026-08-18 20:45:04 +0300

    NextAuth.js Installation And Auth Route Creation

---
+++ src/app/api/auth/[...nextauth]/route.jsx
@@ -0,0 +1,27 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const handler = NextAuth({});
+providers: [
+  CredentialsProvider({
+    name: "Credentials",
+    credentials: {
+      email: { label: "Email", type: "email", placeholder: "Enter Your Email" },
+      password: { label: "Password", type: "password" },
+    },
+    async authorize(credentials, req) {
+      const user = {
+        id: "1",
+        email: "emre@gmail.com",
+        password: "1234",
+      };
+
+      if (user) {
+        return user;
+      } else {
+        return null;
+      }
+    },
+  }),
+];
+export { handler as GET, handler as POST };
