b120fc4ef7da417fb906d47a35323e7cc8f4ba09
Muhammet Emre Numanoglu <muhammedemrenumanoglu155@gmail.com>
2026-08-24 18:42:46 +0300

    Create Category Model

--- src/app/components/nav/header.js
+++ src/app/components/nav/header.js
@@ -1,14 +1,14 @@
 "use client";
 import React from "react";
 import Link from "next/link";
-import { useSession ,signOut} from "next-auth/react";
+import { useSession, signOut } from "next-auth/react";
 export default function NavbarComponent() {
   const { data: session } = useSession();
-  const logOut=()=>{
+  const logOut = () => {
     signOut({
-        callbackUrl:"/"
-    })
-  }
+      callbackUrl: "/",
+    });
+  };
   return (
     <nav className="bg-gray-900 border-slate-400 text-white px-6 py-3">
       <div className="flex justify-between items-center">
@@ -17,22 +17,43 @@ export default function NavbarComponent() {
         </Link>
         <ul className="flex gap-6">
           <li>
-            <Link href="/dashboard">Dashboard</Link>
-          </li>
-          <li>
             <Link href="/blogs">Blogs</Link>
           </li>
 
           {!session ? (
             <li>
-              <Link href="/api/auth/signin">Register</Link>
+              <Link href="/register">Register</Link>
             </li>
           ) : (
-            <li>
-              <button onClick={logOut} className="btn btn-danger bg-red-500 p-2 rounded">
-                Log Out
-              </button>
-            </li>
+            <>
+              <li>
+                <Link href="/dashboard">Dashboard</Link>
+              </li>
+              <li>
+                <Link
+                  className=" bg-blue-500 p-2 rounded"
+                  href="/dashboard/add_course"
+                >
+                  Add Course
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className=" bg-blue-500 p-2 rounded"
+                  href="/dashboard/add_category"
+                >
+                  Add Category
+                </Link>
+              </li>
+              <li>
+                <button
+                  onClick={logOut}
+                  className="btn btn-danger bg-red-500 p-2 rounded"
+                >
+                  Log Out
+                </button>
+              </li>
+            </>
           )}
         </ul>
       </div>
